db4874b97810e6a705dac51d78a22a0b83623aaf
Viviane Desgrange <viviane.desgrange@gmail.com>
2022-08-15 00:44:09 +0200

    Update core code.

--- src/vk_engine.cpp
+++ src/vk_engine.cpp
@@ -1,36 +1,147 @@
-﻿
-#include "vk_engine.h"
+﻿#define GLFW_INCLUDE_VULKAN
 
-#include <SDL.h>
-#include <SDL_vulkan.h>
-
+#include <GLFW/glfw3.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_vulkan.h>
 #include <vk_types.h>
 #include <vk_initializers.h>
+#include <iostream>
+
+#include "vk_engine.h"
+#include "VkBootstrap.h"
+
+
+using namespace std;
+
+#define VK_CHECK(x) \
+    do \
+    { \
+        VkResult err = x; \
+        if (err) { \
+            std::cout <<"Detected Vulkan error: " << err << std::endl; \
+			abort(); \
+        } \
+    } while (0)
+
+void VulkanEngine::init_window() {
+    glfwInit();
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+
+    _window = glfwCreateWindow(_windowExtent.width, _windowExtent.height, "Vulkan", nullptr, nullptr);
+    glfwSetWindowUserPointer(_window, this);
+    glfwSetFramebufferSizeCallback(_window, framebufferResizeCallback);
+}
+
+void VulkanEngine::framebufferResizeCallback(GLFWwindow* window, int width, int height) {
+    auto app = reinterpret_cast<VulkanEngine*>(glfwGetWindowUserPointer(window));
+    app->framebufferResized = true;
+}
 
 void VulkanEngine::init()
 {
 	// We initialize SDL and create a window with it. 
 	SDL_Init(SDL_INIT_VIDEO);
 
-	SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_VULKAN);
-	
-	_window = SDL_CreateWindow(
-		"Vulkan Engine",
-		SDL_WINDOWPOS_UNDEFINED,
-		SDL_WINDOWPOS_UNDEFINED,
-		_windowExtent.width,
-		_windowExtent.height,
-		window_flags
-	);
+    SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_VULKAN);
+
+//    _window = SDL_CreateWindow(
+//		"Vulkan Engine",
+//		SDL_WINDOWPOS_UNDEFINED,
+//		SDL_WINDOWPOS_UNDEFINED,
+//		_windowExtent.width,
+//		_windowExtent.height,
+//		window_flags
+//	);
+
+    init_window();
+    init_vulkan();
+    init_swapchain();
 	
 	//everything went fine
 	_isInitialized = true;
 }
+
+void VulkanEngine::init_vulkan() {
+//    create_instance();
+//    setup_debug_messenger();
+//
+//    pickPhysicalDevice(deviceExtensions);
+//    createLogicalDevice(deviceExtensions, validationLayers, enableValidationLayers);
+
+    vkb::InstanceBuilder builder;
+
+    //make the Vulkan instance, with basic debug features
+    auto inst_ret = builder.set_app_name("Example Vulkan Application")
+            .request_validation_layers(true)
+            .require_api_version(1, 1, 0)
+            .use_default_debug_messenger()
+            .build();
+
+    vkb::Instance vkb_inst = inst_ret.value();
+
+    //store the instance
+    _instance = vkb_inst.instance;
+    //store the debug messenger
+    _debug_messenger = vkb_inst.debug_messenger;
+
+    // get the surface of the window we opened with SDL
+    if (glfwCreateWindowSurface(_instance, _window, nullptr, &_surface) != VK_SUCCESS) {
+        throw std::runtime_error("failed to create window surface!");
+    }
+
+    //use vkbootstrap to select a GPU.
+    //We want a GPU that can write to the SDL surface and supports Vulkan 1.1
+    vkb::PhysicalDeviceSelector selector{ vkb_inst };
+    vkb::PhysicalDevice physicalDevice = selector
+            .set_minimum_version(1, 1)
+            .set_surface(_surface)
+            .select()
+            .value();
+
+    //create the final Vulkan device
+    vkb::DeviceBuilder deviceBuilder{ physicalDevice };
+
+    vkb::Device vkbDevice = deviceBuilder.build().value();
+
+    // Get the VkDevice handle used in the rest of a Vulkan application
+    _device = vkbDevice.device;
+    _physicalDevice = physicalDevice.physical_device;
+
+}
+
+void VulkanEngine::init_swapchain() {
+    vkb::SwapchainBuilder swapchainBuilder{_physicalDevice, _device, _surface };
+
+    vkb::Swapchain vkbSwapchain = swapchainBuilder
+            .use_default_format_selection()
+            .set_desired_present_mode(VK_PRESENT_MODE_FIFO_KHR)
+            .set_desired_extent(_windowExtent.width, _windowExtent.height)
+            .build()
+            .value();
+
+    _swapchain = vkbSwapchain.swapchain;
+    _swapChainImages = vkbSwapchain.get_images().value();
+    _swapChainImageViews = vkbSwapchain.get_image_views().value();
+    _swapChainImageFormat = vkbSwapchain.image_format;
+}
+
 void VulkanEngine::cleanup()
 {	
 	if (_isInitialized) {
-		
-		SDL_DestroyWindow(_window);
+        vkDestroySwapchainKHR(_device, _swapchain, nullptr);
+
+        for (int i = 0; i < _swapChainImageViews.size(); i++) {
+            vkDestroyImageView(_device, _swapChainImageViews[i], nullptr);
+        }
+
+        vkDestroyDevice(_device, nullptr);
+        vkDestroySurfaceKHR(_instance, _surface, nullptr);
+        vkb::destroy_debug_utils_messenger(_instance, _debug_messenger);
+        vkDestroyInstance(_instance, nullptr);
+
+        glfwDestroyWindow(_window);
+        glfwTerminate();
 	}
 }
 
--- src/vk_engine.h
+++ src/vk_engine.h
@@ -1,19 +1,64 @@
-﻿// vulkan_guide.h : Include file for standard system include files,
-// or project specific include files.
+﻿#pragma once
 
-#pragma once
+#ifndef GLFW_INCLUDE_VULKAN
+#define GLFW_INCLUDE_VULKAN
+#endif
 
+#include <GLFW/glfw3.h>
+#include <vector>
+#include <set>
+#include <string>
+#include <optional>
 #include <vk_types.h>
+
+const bool enableValidationLayers = true;
+
+const std::vector<const char*> validationLayers = {
+        "VK_LAYER_KHRONOS_validation"
+};
+
+const std::vector<const char*> deviceExtensions = {
+        VK_KHR_SWAPCHAIN_EXTENSION_NAME
+};
+
+struct SwapChainSupportDetails {
+    VkSurfaceCapabilitiesKHR capabilities;
+    std::vector<VkSurfaceFormatKHR> formats;
+    std::vector<VkPresentModeKHR> presentModes;
+};
+
+struct QueueFamilyIndices {
+    std::optional<uint32_t> graphicsFamily;
+    std::optional<uint32_t> presentFamily;
+
+    bool isComplete() {
+        return graphicsFamily.has_value() && presentFamily.has_value();
+    }
+};
 
 class VulkanEngine {
 public:
-
 	bool _isInitialized{ false };
 	int _frameNumber {0};
 
+    // struct SDL_Window* _window{ nullptr };
+    GLFWwindow* _window;
 	VkExtent2D _windowExtent{ 1700 , 900 };
+    bool framebufferResized = false;
 
-	struct SDL_Window* _window{ nullptr };
+    VkInstance _instance; // Vulkan library handle
+    VkDebugUtilsMessengerEXT _debug_messenger; // Vulkan debug output handle
+    VkPhysicalDevice _physicalDevice; // GPU chosen as the default device
+    VkDevice _device; // Vulkan device for commands
+    VkSurfaceKHR _surface; // Vulkan window surface
+
+    VkQueue graphicsQueue;
+    VkQueue presentQueue;
+
+    VkSwapchainKHR _swapchain;
+    VkFormat _swapChainImageFormat;
+    std::vector<VkImage> _swapChainImages;
+    std::vector<VkImageView> _swapChainImageViews;
 
 	//initializes everything in the engine
 	void init();
@@ -26,4 +71,45 @@ public:
 
 	//run main loop
 	void run();
+
+private:
+    // Initialize vulkan
+    void init_vulkan();
+
+    void init_window();
+
+    static void framebufferResizeCallback(GLFWwindow* window, int width, int height);
+
+    // Initialize swapchain to render on-screen images
+    void init_swapchain();
+//
+//    std::vector<const char*> get_required_extensions();
+//
+//    bool check_validation_layer_support(const std::vector<const char*>& validationLayers);
+//
+//    void populate_debug_messenger_create_info(VkDebugUtilsMessengerCreateInfoEXT& createInfo);
+//
+//    static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
+//            VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+//            VkDebugUtilsMessageTypeFlagsEXT messageType,
+//            const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+//            void *pUserData);
+//
+//    void setup_debug_messenger();
+//
+//    void create_instance();
+
+//    void createLogicalDevice(const std::vector<const char*>& deviceExtensions,
+//                                           const std::vector<const char*> validationLayers,
+//                                           const bool enableValidationLayers);
+//
+//    void pickPhysicalDevice(const std::vector<const char*>& deviceExtensions);
+//
+//    bool isDeviceSuitable(VkPhysicalDevice device, const std::vector<const char*>& deviceExtensions);
+//
+//    bool checkDeviceExtensionSupport(VkPhysicalDevice device, const std::vector<const char*>& deviceExtensions);
+//
+//    SwapChainSupportDetails querySwapChainSupport(VkPhysicalDevice& device, VkSurfaceKHR& surface);
+//
+//    QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device);
 };
