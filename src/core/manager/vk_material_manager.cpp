b4d94ad566b24c881919e51056061496135c9926
Viviane Desgrange <viviane.desgrange@gmail.com>
2024-03-24 14:08:38 +0100

    Add method with pipelineBuilder pass as argument.

--- src/core/manager/vk_material_manager.cpp
+++ src/core/manager/vk_material_manager.cpp
@@ -27,7 +27,8 @@ MaterialManager::~MaterialManager() {
 //            material->pipelineLayout = VK_NULL_HANDLE;
 //        }
 //    }
-    _entities.clear();
+   _entities.clear();
+   _discarded.clear();
 }
 
 std::shared_ptr<Material> MaterialManager::get_material(const std::string &name) {
@@ -57,6 +58,47 @@ std::shared_ptr<Material> MaterialManager::create_material(std::string name, std
 
     std::shared_ptr<ShaderEffect> effect = _pipelineBuilder->build_effect(setLayouts, pushConstants, modules);
     std::shared_ptr<ShaderPass> pass = _pipelineBuilder->build_pass(effect);
+
+    if (this->_entities.count(name) != 0) {
+        this->_discarded.emplace(name, std::move(this->get_entity(name)));
+    }
+
+    this->add_entity(name, pass);
+
+    for (auto& shader : effect->shaderStages) {
+        vkDestroyShaderModule(_device->_logicalDevice, shader.shaderModule, nullptr);
+    }
+
+    return pass;
+}
+
+std::shared_ptr<Material> MaterialManager::create_material(PipelineBuilder& pipelineBuilder, std::string name, std::vector<VkDescriptorSetLayout> setLayouts, std::vector<PushConstant> constants, std::vector<std::pair<ShaderType, const char*>> shaders, std::unordered_map<ShaderType, VkSpecializationInfo> shaderSpecialization) {
+    std::vector<VkPushConstantRange> pushConstants {};
+    uint32_t offset = 0;
+    for (const auto& p: constants) {
+        VkPushConstantRange push_model;
+        push_model.offset = offset;
+        push_model.size = p.size;
+        push_model.stageFlags = Shader::get_shader_stage(p.stage);
+
+        pushConstants.push_back(push_model);
+        offset += p.size;
+    }
+
+    std::vector<std::tuple<VkShaderStageFlagBits, const char*, VkSpecializationInfo>> modules {};
+    modules.reserve(shaders.size());
+    for (const auto& it: shaders) {
+        VkSpecializationInfo third = shaderSpecialization[it.first];
+        modules.emplace_back(std::make_tuple(Shader::get_shader_stage(it.first), it.second, third));
+    }
+
+    std::shared_ptr<ShaderEffect> effect = pipelineBuilder.build_effect(setLayouts, pushConstants, modules);
+    std::shared_ptr<ShaderPass> pass = pipelineBuilder.build_pass(effect);
+
+    if (this->_entities.count(name) != 0) {
+        this->_discarded.emplace(name, std::move(this->get_entity(name)));
+    }
+
     this->add_entity(name, pass);
 
     for (auto& shader : effect->shaderStages) {
--- src/core/manager/vk_material_manager.h
+++ src/core/manager/vk_material_manager.h
@@ -24,10 +24,17 @@ public:
 
     MaterialManager(const Device* _device, PipelineBuilder* pipelineBuilder);
     ~MaterialManager();
+    MaterialManager(const MaterialManager&) = delete;
+    MaterialManager(MaterialManager&&) noexcept = default;
+    MaterialManager& operator=(const MaterialManager&) = delete;
+    MaterialManager& operator=(MaterialManager&&) noexcept = default;
 
     std::shared_ptr<Material> get_material(const std::string &name);
     std::shared_ptr<Material> create_material(std::string name, std::vector<VkDescriptorSetLayout> setLayouts, std::vector<PushConstant> constants, std::vector<std::pair<ShaderType, const char*>> shaders, std::unordered_map<ShaderType, VkSpecializationInfo> shaderSpecialization = {});
+    std::shared_ptr<Material> create_material(PipelineBuilder& pipelineBuilder, std::string name, std::vector<VkDescriptorSetLayout> setLayouts, std::vector<PushConstant> constants, std::vector<std::pair<ShaderType, const char*>> shaders, std::unordered_map<ShaderType, VkSpecializationInfo> shaderSpecialization = {});
 
 private:
     const class Device* _device;
+    /** @brief Maintain obsolete materials until command buffer stop running */
+    std::unordered_map<std::string, std::shared_ptr<Entity>> _discarded;
 };
